70a22fa6821982214f60efe6289acfc9eeb1aac0
Gabrielli Correa <gabriellicorrea7@gmail.com>
2021-05-26 20:39:31 -0300

    ajustes

--- proposta/src/main/java/br/com/zup/gabrielli/propostas/seguranca/DadosConverter.java
+++ proposta/src/main/java/br/com/zup/gabrielli/propostas/seguranca/DadosConverter.java
@@ -8,12 +8,13 @@ import org.springframework.security.crypto.encrypt.Encryptors;
 @Converter
 public class DadosConverter implements AttributeConverter<String, String> {
 
-	public String convertToDatabaseColumn(String dado) {
-		return Encryptors.text("${criptografia.secret}", "574bed3ab0c9c7").encrypt(dado);
-	}
+    @Override
+    public String convertToDatabaseColumn(String dado) {
+        return Encryptors.text("${criptografia.secret}", "574bed3ab0c9c7").encrypt(dado);
+    }
 
-	@Override
-	public String convertToEntityAttribute(String dado) {
-		return Encryptors.text("${criptografia.secret}", "574bed3ab0c9c7").decrypt(dado);
-	}
+    @Override
+    public String convertToEntityAttribute(String dado) {
+        return Encryptors.text("${criptografia.secret}", "574bed3ab0c9c7").decrypt(dado);
+    }
 }
